31f50359d98f93c6fba8d40ee23f602452c2fd04
SHsteak <cho911115@naver.com>
2019-08-02 18:05:13 +0900

    기존에 풀었던 문제 등록

---
+++ Programmers/위장/Solution.java
@@ -0,0 +1,28 @@
+﻿/*
+ * About
+ * Author: SHsteak@GitHub <cho911115@naver.com>
+ * Date : 2019-08-02
+ * URL : https://programmers.co.kr/learn/courses/30/lessons/42578
+ */
+
+import java.util.HashMap;
+public class Solution {
+	public static void main(String[] args) {
+		String[][] test_case = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+		System.out.println(new Solution().solution(test_case));
+		// TODO Auto-generated method stub
+	}
+	
+	public int solution(String[][] clothes) {
+		int answer = 1;
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		for (int i = 0; i < clothes.length; i++) {
+			String c = clothes[i][1];
+			if (hm.putIfAbsent(c, 1) != null) hm.put(c, hm.get(c) + 1);
+		}
+		for (int v : hm.values()) {
+			answer *= v + 1;
+		}
+		return answer - 1;
+	}
+}
