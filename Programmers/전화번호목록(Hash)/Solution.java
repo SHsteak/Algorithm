6d60036b6c5f30de786e29fbba8f3e2dcdd51e86
SHsteak <cho911115@naver.com>
2019-08-13 16:09:33 +0900

    add

---
+++ Programmers/전화번호목록(Hash)/Solution.java
@@ -0,0 +1,38 @@
+﻿/*
+ * About
+ * Author: SHsteak@GitHub <cho911115@naver.com>
+ * Date : 2019-08-13
+ * URL : https://programmers.co.kr/learn/courses/30/lessons/42577
+ */
+import java.util.HashMap;
+public class Solution {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] t = {"119", "97674223", "1195524421"};
+		String[] t2 = {"123", "456", "789"};
+		String[] t3 = {"12", "123", "1235", "567", "88"};
+		System.out.println(solution(t));
+		System.out.println(solution(t2));
+		System.out.println(solution(t3));
+	}
+	
+	public static boolean solution(String[] phone_book) {
+		boolean answer = true;
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		for (String p : phone_book) {
+			hm.put(p, p.length());
+		}
+		for (String key : hm.keySet()) {
+			for (String key2 : hm.keySet()) {
+				if (hm.get(key) <= hm.get(key2) && !key.equals(key2)) {
+					if (key.equals(key2.substring(0, hm.get(key)))) {
+						answer = false;
+						break;
+					}
+				}
+			}
+			if (!answer) break;
+		}
+		return answer;
+	}
+}
---
+++ Programmers/타겟넘버(DFS, BFS)/Solution.java
@@ -0,0 +1,25 @@
+﻿/*
+ * About
+ * Author: SHsteak@GitHub <cho911115@naver.com>
+ * Date : 2019-08-13
+ * URL : https://programmers.co.kr/learn/courses/30/lessons/43165
+ */
+public class Solution {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 1, 1, 1, 1};
+		System.out.println(new Solution().solution(arr, 3));
+	}
+	
+	public int solution(int[] numbers, int target) {
+		return DFS(numbers, target, 0, 0);
+	}
+	
+	private int DFS(int[] numbers, int target, int idx, int result) {
+		if (idx == numbers.length) {
+			return result == target ? 1 : 0;
+		} else {
+			return DFS(numbers, target, idx + 1, result + numbers[idx]) + DFS(numbers, target, idx + 1, result - numbers[idx]);
+		}
+	}
+}
